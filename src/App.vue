69ee7cb71fced79f52fb47520510e835a7317921
Jorge de la Torre <jorge_dlt1992@hotmail.com>
2019-12-12 12:16:14 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <h1>Todos</h1>
+    <input type="text" v-model="todoName" @keyup.enter="addTodo">
+    <ul>
+      <li v-for="todo of todos" :key="todo.id">
+        {{todo.name}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3000/todos';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      todos: [],
+      todoName: ''
+    }
+  },
+  async created() {
+    try {
+      const res = await axios.get(baseUrl) ;
+
+      this.todos = res.data;
+  } catch(e) {
+      console.error(e);
+    }
+  },
+  methods: {
+    async addTodo() {
+      const res = await axios.post(baseUrl, {name: this.todoName})
+
+      this.todos = [...this.todos, res.data];
+
+      this.todoName = '';
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+ul {
+  margin: 0px;
+  padding: 0px;
+}
+
+li {
+  list-style: none;
+}
+</style>
